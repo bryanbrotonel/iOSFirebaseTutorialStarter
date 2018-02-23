3e1b9f45b5845f1becd9e948944e51596f027700
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-02-23 11:37:43 -0800

    Initial Commit

---
+++ iOSFirebaseTutorialStarter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSFirebaseTutorialStarter
+//
+//  Created by Bryan Brotonel on 2018-02-23.
+//  Copyright © 2018 Bryan Brotonel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
